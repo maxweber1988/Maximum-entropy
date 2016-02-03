188342f4969bc7204c1009d53fd96840caff4d38
Michael Haslauer <michael.haslauer@gmx.de>
2016-02-03 05:10:18 +0100

    Michis Version

--- code/max_ent.py
+++ code/max_ent.py
@@ -1,13 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-from max_ent_functions import calc_A, calc_G, calc_K, root_finding_newton, root_finding_diag
+from max_ent_functions import calc_A, calc_G, calc_K, root_finding_newton, root_finding_diag, max_likelihood_estimate
+
+
 plt.rc('image',interpolation='nearest',origin='lower')
 # dummy change
 # define parameters
 dtau = 0.125
-dw = 0.25
-Nw = 40
+dw = 0.125
+Nw = 80
 m_value = 1./(Nw*dw)
 m = np.zeros(Nw) + m_value
 beta = 10.
@@ -15,8 +17,8 @@ Ntau = int((beta/2.)  / dtau)
 sigma = 2.
 # create dummy A and calculate K,G
 x = np.arange(0.,10.,dw)
-A = np.exp(-(x-(beta)/2.)**2/sigma**2)
-A += 0.5*np.exp(-(x-(beta+5)/2.)**2/sigma**2)
+A = np.exp(-((x-3)**2)/0.8)
+A += 0.25*np.exp(-((x-6)**2)/2)
 A = A * dw
 plt.plot(A)
 plt.figure()
@@ -27,17 +29,17 @@ plt.figure()
 plt.plot(G)
 plt.show()
 # transform K,G and add additional noise to G
-G_noisy = G + np.random.normal(0.,1e-10,len(G))
-Cov = np.diag(np.ones(len(G))*1e-10)
+G_noisy = G + np.random.normal(0.,1e-6,len(G))
+Cov = np.diag(np.ones(len(G))*1e-4)
 
-# singular value decomposition of K_diag = V * Sigma * transpose(U)
+# singular value decomposition of K = V * Sigma * transpose(U)
 V,sig_vec,U_T = np.linalg.svd(K)
 
 # create sigma matrix for convenience out of sig_vec
 Sigma = np.diag(sig_vec)
 
 # find important singular values and reduce dimensions accordingly
-s = len(sig_vec[sig_vec>1e-3])
+s = len(sig_vec[sig_vec>1e-6])
 U = U_T.T
 U_s = U[:,0:s]
 V_s = V[:,0:s]
@@ -46,28 +48,33 @@ Sigma_s = Sigma[0:s,0:s]
 K_s = np.dot(V_s,np.dot(Sigma_s,U_s.T))
 p_alpha = []
 delta_A = []
-for i in xrange(1):
-	alpha = i + 1.
+for i in range(1):
+	alpha =  0
 	# create starting values for u and start root finding
 	u=np.zeros(s)+ 1.
 	delta_u = []
-	# find solution u recusively
-	for j in xrange(3):
+	# find solution u recursively, why?
+	for j in range(3):
 		u_sol= root_finding_newton(u = u,m = m, alpha = alpha, V = V_s, Sigma = Sigma_s,U = U_s, G = G_noisy, Cov = Cov, dw = dw)
 		delta_u.append(np.abs(np.sum(u-u_sol)))
 		u=u_sol
 	A_est = m * np.exp(np.dot(U_s, u_sol)) * dw
 
-	S = np.sum(A_est - m - np.log(A_est / m))
-	L = 0.5 * np.sum((G_noisy - np.dot(K_s,A_est))**2/np.diagonal(Cov)**2)
-	Q = alpha * S - L
-	p_alpha_val = np.prod(np.sqrt(alpha/(alpha+Lambda) * 1./alpha * np.exp(Q)))
-	p_alpha.append(p_alpha_val)
-	delta_A.append(np.sum(np.sqrt((A-A_est)**2)))
+	#S = np.sum(A_est - m - np.log(A_est / m))
+	#L = 0.5 * np.sum((G_noisy - np.dot(K_s,A_est))**2/np.diagonal(Cov)**2)
+	#Q = alpha * S - L
+	#p_alpha_val = np.prod(np.sqrt(alpha/(alpha+Lambda) * 1./alpha * np.exp(Q)))
+	#p_alpha.append(p_alpha_val)
+	#delta_A.append(np.sum(np.sqrt((A-A_est)**2)))
+
+#calculate ML estimate
+
+A_ML = max_likelihood_estimate(G_noisy,V_s,U_s,Sigma_s)
 
 plt.plot(A_est,'rx',label='A_est')
 plt.plot(A,'b+',label='true A')
+plt.plot(A_ML,'kx',label='ML A')
 plt.legend()
-plt.figure()
-plt.plot(p_alpha)
+#plt.figure()
+#plt.plot(p_alpha)
 plt.show()
--- code/max_ent_functions.py
+++ code/max_ent_functions.py
@@ -6,55 +6,118 @@ def calc_A(dw,N,peak_indices,peak_width,heights):
 	value_number = np.arange(-peak_width/2,peak_width/2+1,1)
 	weight_step = 1. / (len(value_number)/2)
 	weights = np.arange(-1.,1.+weight_step,weight_step)
-	for i in xrange(len(peak_indices)):
+	for i in range(len(peak_indices)):
 		res[peak_indices[i]-peak_width/2:peak_indices[i]+peak_width/2+1] = heights[i] - heights[i]*np.abs(np.round(weights,8))
 	return res
 
 def calc_K(dw,Nw,dtau,Ntau,beta):
+	"""calculates the kernel matrix for given number of time steps Ntau, and omega steps Nw.\
+	first index gives the time step\
+	second index gives the omega step.\
+	beta: period of the Greens function, needed for calculatin of the Kernel.\
+	dtau,dw: step sizes for imaginary time tau and frequency omega."""
+
 	res = np.zeros((Ntau,Nw))
-	for i in xrange(0,Ntau):
-		for j in xrange(0,Nw):
+
+	for i in range(0,Ntau):
+		for j in range(0,Nw):
 			res[i,j] = np.exp(-j * dw * i * dtau) / (1. + np.exp(-beta * (j * dw)))
 	return res
 
 def calc_G(K,A):
+	"""
+	:param K: Kernel Matrix
+	:param A: Vector representation of the Lehmann spectral function
+	:return: resulting greens function given K and A
+	"""
 	res = np.dot(K,A)
 	return res
 
 def root_finding_newton(u, m, alpha, V, Sigma, U, G, Cov, dw):
-	s=len(u)
-	max_val = np.sum(m)
-	K_s = np.dot(V,np.dot(Sigma,U.T))
-	for i in xrange(1000):
-		A_appr = dw * m * np.exp(np.dot(U,u))
-		inv_cov = (1./np.diagonal(Cov))**2
-		inv_cov_mat = np.diag(inv_cov)
-		dLdF = - inv_cov * (G - np.dot(K_s, A_appr))
-		F_u = - alpha * u - np.dot(Sigma,np.dot(V.T,dLdF))
-		M = np.dot(Sigma,np.dot(V.T,np.dot(inv_cov_mat,np.dot(V,Sigma))))
-		T = np.dot(U.T,np.dot(np.diag(A_appr),U))
-		J = alpha * np.diag(np.ones((s))) + np.dot(M,T)
+	"""
+	:param u: initial vector of u
+	:param m: vector of default model for the Lehmann spectral function
+	:param alpha: scalar value, controls the relative weight of maximizing entropie and minimizing kind. of least squares fit.
+	:param V: part of singular SVD of K, K = V*Sigma*U.T
+	:param Sigma: part of singular SVD of K, K = V*Sigma*U.T
+	:param U: part of singular SVD of K, K = V*Sigma*U.T
+	:param G: Vector of all average values of the Greensfunction for each time step
+	:param Cov: Vector with variance of the re-binned, gaussian distributed QMC approximations for the different time-steps
+	:param dw: omega step size
+	:return:
+	"""
 
+	# dimension of singular space
+	s=len(u)
+	# maximum iteration step-size, makes sense according to paper of Bryan
+	max_val = np.sum(m) #
+
+	## setup matrices constant during the loop calculation
+	# recalculate Kernel matrix from its SVD
+	K_s = np.dot(V,np.dot(Sigma,U.T))
+
+	# create the inverse covariance matrix
+	inv_cov = (1./np.diagonal(Cov))**2
+	inv_cov_mat = np.diag(inv_cov)
+
+	# create constant help-matrix for the calculation of the jacobian, J = alpha * 1 + M*X
+	M = np.dot(Sigma,np.dot(V.T,np.dot(inv_cov_mat,np.dot(V,Sigma))))
+
+	for i in range(1000):
+		## set up matrices varying during the loop
+		A_appr = dw * m * np.exp(np.dot(U,u))
+			# we need to multiply by dw since in the definition of u the real function A(w) was used and not the discretized
+			# form for as used to approximate the integral as a Riemann sum.
+
+		dLdF = - inv_cov * (G - np.dot(K_s, A_appr))
+		F_u = -0 * u - np.dot(Sigma,np.dot(V.T,dLdF)) #reset 0 to alpha
+		X = np.dot(U.T,np.dot(np.diag(A_appr),U))
+
+		# initialize jacobian
+		J = 0 * np.diag(np.ones((s))) + np.dot(M,X) #reset 0 to alpha
+
+		# solve system of linear equations Jdu = -F_u for du
+		# using th LU factorization
 		lu_and_piv = lu_factor(J)
 		delta_u = lu_solve(lu_and_piv,F_u)
-		for j in xrange(1000):
-			if np.dot(delta_u.T,np.dot(T,delta_u.T)) > max_val:
-				J = (alpha+j) * np.diag(np.ones((s))) + np.dot(M,T)
+
+		# check if iteration step size is small enough, otherwise adjust parameters such that step size will be smaller
+		for j in range(1000):
+			if np.dot(delta_u.T,np.dot(X,delta_u)) > max_val:
+				J = (alpha+j) * np.diag(np.ones((s))) + np.dot(M,X) # adjusted according to Brayn paper
 				lu_and_piv = lu_factor(J)
 				delta_u = lu_solve(lu_and_piv,F_u)
 			else:
 				break
 		u_old = u
 		u = u + delta_u
-		if np.abs(np.sum(u-u_old))<1e-10:
+		if np.abs(np.sum(u-u_old))<1e-10: # maybe criterion from Brayn paper, p170
+			print('Maximum accuracy was reached!')
 			break
 	return u	
+
+
+def max_likelihood_estimate(G,V_singular,U_singular,Sigma_singular):
+	"""
+	:param G: Vector of all average values of the Greensfunction for each time step
+	:param V_singular: part of the SVD of K , K = V*Sigma.T*U.T which contains only the components for non zero singular values.
+	:param U_singular: part of the SVD of K , K = V*Sigma.T*U.T which contains only the components for non zero singular values.
+	:param Sigma_singular: part of the SVD of K , K = V*Sigma.T*U.T which contains only the components for non zero singular values.
+	:return: Maximum likelihood estimate of the Lehmann spectral function A.
+	"""
+
+	inv_Sigma_singular = np.linalg.inv(Sigma_singular)
+
+	inv_K_singular = np.dot(U_singular,np.dot(inv_Sigma_singular,V_singular.T))
+
+	return np.dot(inv_K_singular,G)
+
 
 def root_finding_diag(u, m, alpha, V, Sigma, U, G, Cov, dw):
 	s=len(u)
 	max_val = np.sum(m)
 	T_s = np.dot(V,np.dot(Sigma,U.T))
-	for i in xrange(1000):
+	for i in range(1000):
 		f_appr = dw * m * np.exp(np.dot(U,u))
 		inv_cov = (1./np.diagonal(Cov))**2
 		inv_cov_mat = np.diag(inv_cov)
@@ -72,7 +135,7 @@ def root_finding_diag(u, m, alpha, V, Sigma, U, G, Cov, dw):
 		Lambda = np.diag(eig_A)
 		Y = np.dot(P,np.dot(np.sqrt(O_inv),R))
 		Y_inv = np.linalg.inv(Y)
-		for j in xrange(1000):
+		for j in range(1000):
 			B = (alpha+j)*np.diag(np.ones((s)))+Lambda
 			C = -alpha * np.dot(Y_inv,u)-np.dot(Y_inv,g)
 			lu_and_piv = lu_factor(B)
