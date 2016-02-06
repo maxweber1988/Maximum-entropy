72fc45bc8ff5388dd9a7ac8c6b566161e7a2a5e5
Max Weber <max.weber@posteo.de>
2016-02-06 18:59:11 +0100

    bugfixes

--- code/max_ent.py
+++ code/max_ent.py
@@ -1,73 +1,56 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import time
-from max_ent_functions import calc_A, calc_G, calc_K, root_finding_newton, root_finding_diag
+from max_ent_functions import calc_A, calc_K, root_finding_newton, root_finding_diag,calc_p_alpha
 plt.rc('image',interpolation='nearest',origin='lower')
-# dummy change
+
 # define parameters
-dtau = 0.125
-dw = 0.25
-Nw = 40
+beta = 10.
+Ntau = 200
+Nw = 200
+dtau = (beta/2.)/Ntau
+dw = 10./Nw
 m_value = 1./(Nw*dw)
 m = np.zeros(Nw) + m_value
-beta = 10.
-Ntau = int((beta/2.)  / dtau)
-sigma = 2.
-# create dummy A and calculate K,G
-x = np.arange(0.,10.,dw)
-A = np.exp(-(x-(beta)/2.)**2/sigma**2)
-A += 0.5*np.exp(-(x-(beta+5)/2.)**2/sigma**2)
-A = A * dw
+# create dummy A
+mu = np.array([-1.,1.])
+sigma = [1.,2.]
+ampl = [2.,1.]
+A = calc_A(dw,Nw,mu,sigma,ampl)
 plt.plot(A)
-plt.figure()
+# calculate K and G
 K = calc_K(dw,Nw,dtau,Ntau,beta)
+plt.figure()
 plt.imshow(K)
-G = calc_G(K,A)
+G = np.dot(K,A)
 plt.figure()
 plt.plot(G)
 plt.show()
-# transform K,G and add additional noise to G
-G_noisy = G + np.random.normal(0.,1e-10,len(G))
-Cov = np.diag(np.ones(len(G))*1e-10)
+# transform K,G if necessary and add additional noise to G
+G_noisy = G + np.random.normal(0.,1e-5,len(G))
 
-# singular value decomposition of K_diag = V * Sigma * transpose(U)
+Cov = np.diag(np.ones(len(G))*1e-5)
+
+# singular value decomposition of K = V * Sigma * transpose(U)
 V,sig_vec,U_T = np.linalg.svd(K)
-
 # create sigma matrix for convenience out of sig_vec
 Sigma = np.diag(sig_vec)
 
-# find important singular values and reduce dimensions accordingly
-s = len(sig_vec[sig_vec>1e-3])
+s = len(sig_vec[sig_vec>10**(-8.)])
 U = U_T.T
 U_s = U[:,0:s]
 V_s = V[:,0:s]
 Sigma_s = Sigma[0:s,0:s]
-
 K_s = np.dot(V_s,np.dot(Sigma_s,U_s.T))
-p_alpha = []
-delta_A = []
-for i in xrange(1):
-	alpha = i + 1.
-	# create starting values for u and start root finding
-	u=np.zeros(s)+ 1.
-	delta_u = []
-	# find solution u recusively
-	for j in xrange(3):
-		u_sol= root_finding_newton(u = u,m = m, alpha = alpha, V = V_s, Sigma = Sigma_s,U = U_s, G = G_noisy, Cov = Cov, dw = dw)
-		delta_u.append(np.abs(np.sum(u-u_sol)))
-		u=u_sol
-	A_est = m * np.exp(np.dot(U_s, u_sol)) * dw
-
-	S = np.sum(A_est - m - np.log(A_est / m))
-	L = 0.5 * np.sum((G_noisy - np.dot(K_s,A_est))**2/np.diagonal(Cov)**2)
-	Q = alpha * S - L
-	p_alpha_val = np.prod(np.sqrt(alpha/(alpha+Lambda) * 1./alpha * np.exp(Q)))
-	p_alpha.append(p_alpha_val)
-	delta_A.append(np.sum(np.sqrt((A-A_est)**2)))
-
-plt.plot(A_est,'rx',label='A_est')
-plt.plot(A,'b+',label='true A')
-plt.legend()
-plt.figure()
-plt.plot(p_alpha)
+alpha = .05
+# create starting values for u and start root finding recursively
+u=np.zeros(s)+ 1.
+for j in xrange(100):
+	u_sol= root_finding_diag(u = u,m = m, alpha = alpha, V = V_s, Sigma = Sigma_s,U = U_s, G = G_noisy, Cov = Cov, dw = dw)
+	u = u_sol
+# estimated A given solution of root finding
+A_est = m * np.exp(np.dot(U_s, u_sol))*dw
+plt.plot(A,'-x',label='true A')
+plt.plot(A_est,label='estimated A')
+plt.legend(loc='best')
 plt.show()
--- code/max_ent_functions.py
+++ code/max_ent_functions.py
@@ -1,85 +1,124 @@
 import numpy as np
 from scipy.linalg import lu_solve,lu_factor
+import matplotlib.pyplot as plt
 
-def calc_A(dw,N,peak_indices,peak_width,heights):
-	res = np.zeros(N)
-	value_number = np.arange(-peak_width/2,peak_width/2+1,1)
-	weight_step = 1. / (len(value_number)/2)
-	weights = np.arange(-1.,1.+weight_step,weight_step)
-	for i in xrange(len(peak_indices)):
-		res[peak_indices[i]-peak_width/2:peak_indices[i]+peak_width/2+1] = heights[i] - heights[i]*np.abs(np.round(weights,8))
-	return res
+def calc_A(dw, Nw, mu, sigma,ampl):
+	if not isinstance(mu,(list,np.ndarray)) & isinstance(sigma,(list,np.ndarray)) & isinstance(ampl,(list,np.ndarray)):
+		raise(TypeError('mu, sigma and ampl should all be either a list or numpy array'))
+	if len(mu) != len(sigma) & len(mu) != len(ampl):
+		raise(IndexError("mu, sigma and ampl have to have the same length"))
+	A = np.zeros((Nw))
+	w = np.arange(-Nw*dw/2.,Nw*dw/2.,dw)
+	for i in xrange(len(mu)):
+		A += ampl[i] * np.exp(-((w - mu[i])**2/sigma[i]**2))
+	return A * dw
 
 def calc_K(dw,Nw,dtau,Ntau,beta):
+
 	res = np.zeros((Ntau,Nw))
+
 	for i in xrange(0,Ntau):
 		for j in xrange(0,Nw):
 			res[i,j] = np.exp(-j * dw * i * dtau) / (1. + np.exp(-beta * (j * dw)))
-	return res
 
-def calc_G(K,A):
-	res = np.dot(K,A)
 	return res
 
 def root_finding_newton(u, m, alpha, V, Sigma, U, G, Cov, dw):
+
 	s=len(u)
 	max_val = np.sum(m)
 	K_s = np.dot(V,np.dot(Sigma,U.T))
-	for i in xrange(1000):
+	diff = 1.
+	count1 = 1
+	max_iter = 1000
+
+	while diff > 1e-10 and count1 <= max_iter:
+		print count1
 		A_appr = dw * m * np.exp(np.dot(U,u))
-		inv_cov = (1./np.diagonal(Cov))**2
+		inv_cov = (1. / np.diagonal(Cov)**2)
 		inv_cov_mat = np.diag(inv_cov)
 		dLdF = - inv_cov * (G - np.dot(K_s, A_appr))
 		F_u = - alpha * u - np.dot(Sigma,np.dot(V.T,dLdF))
 		M = np.dot(Sigma,np.dot(V.T,np.dot(inv_cov_mat,np.dot(V,Sigma))))
 		T = np.dot(U.T,np.dot(np.diag(A_appr),U))
 		J = alpha * np.diag(np.ones((s))) + np.dot(M,T)
-
 		lu_and_piv = lu_factor(J)
 		delta_u = lu_solve(lu_and_piv,F_u)
-		for j in xrange(1000):
-			if np.dot(delta_u.T,np.dot(T,delta_u.T)) > max_val:
-				J = (alpha+j) * np.diag(np.ones((s))) + np.dot(M,T)
-				lu_and_piv = lu_factor(J)
-				delta_u = lu_solve(lu_and_piv,F_u)
-			else:
-				break
-		u_old = u
+		count2 = 1
+		while np.dot(delta_u.T,np.dot(T,delta_u.T)) > max_val and count2 <= max_iter:
+			J = (alpha+count2*1e10) * np.diag(np.ones((s))) + np.dot(M,T)
+			if count2 == max_iter:
+				print count2
+			lu_and_piv = lu_factor(J)
+			delta_u = lu_solve(lu_and_piv,F_u)
+			count2 +=1
+		u_old = u 
 		u = u + delta_u
-		if np.abs(np.sum(u-u_old))<1e-10:
-			break
+		diff = np.abs(np.sum(u-u_old))
+		count1 += 1
 	return u	
 
 def root_finding_diag(u, m, alpha, V, Sigma, U, G, Cov, dw):
 	s=len(u)
 	max_val = np.sum(m)
 	T_s = np.dot(V,np.dot(Sigma,U.T))
-	for i in xrange(1000):
+	diff = 1.
+	max_iter1 = 1000
+	max_iter2 = 10000
+	count1 = 1
+	while diff > 1e-10 and count1 < max_iter1:
+		# print count1
 		f_appr = dw * m * np.exp(np.dot(U,u))
-		inv_cov = (1./np.diagonal(Cov))**2
+		if np.any(np.isnan(f_appr)):
+			return np.zeros(u.shape)
+		inv_cov = (1./np.diagonal(Cov))
 		inv_cov_mat = np.diag(inv_cov)
 		dLdF = - inv_cov * (G - np.dot(T_s, f_appr))
 		g = np.dot(Sigma,np.dot(V.T,dLdF))
 		F_u = - alpha * u - g
 		M = np.dot(Sigma,np.dot(V.T,np.dot(inv_cov_mat,np.dot(V,Sigma))))
 		K = np.dot(U.T,np.dot(np.diag(f_appr),U))
+		if np.any(np.isnan(K)):
+			print "Nan values encountered in K"
+			return np.zeros(u.shape)
 		eig_K, P = np.linalg.eig(K)
-		P_inv = np.linalg.inv(P)
 		O = np.diag(eig_K)
-		O_inv = np.linalg.inv(O)
+		if len(eig_K[eig_K<0.]):
+			print eig_K
 		A = np.dot(np.sqrt(O), np.dot(P.T, np.dot(M, np.dot(P, np.sqrt(O)))))
 		eig_A,R = np.linalg.eig(A)
 		Lambda = np.diag(eig_A)
-		Y = np.dot(P,np.dot(np.sqrt(O_inv),R))
-		Y_inv = np.linalg.inv(Y)
-		for j in xrange(1000):
-			B = (alpha+j)*np.diag(np.ones((s)))+Lambda
-			C = -alpha * np.dot(Y_inv,u)-np.dot(Y_inv,g)
-			lu_and_piv = lu_factor(B)
-			Y_inv_delta_u = lu_solve(lu_and_piv,C)# np.dot(np.linalg.inv(B),C)#
-			delta_u = (-alpha * u - g - np.dot(M,np.dot(Y_inv.T,Y_inv_delta_u)))/(alpha+j)
-			if np.linalg.norm(np.dot(Y_inv,delta_u)) < max_val:
-				break
+		Y_inv = np.dot(R.T,np.dot(np.sqrt(O),P.T))
+		B = (alpha)*np.diag(np.ones((s))) + Lambda
+		c_vec = -alpha * np.dot(Y_inv,u)-np.dot(Y_inv,g)
+		Y_inv_delta_u = np.zeros(len(c_vec))
+		for i in xrange(len(c_vec)):
+			Y_inv_delta_u[i] = c_vec[i] / B[i,i]
+
+		delta_u = (-alpha * u - g - np.dot(M,np.dot(Y_inv.T,Y_inv_delta_u)))/(alpha)
+		count2 = 1
+		while np.dot(delta_u.T,np.dot(K,delta_u)) > max_val and count2 < max_iter2:
+			B = (alpha+count2*1.)*np.diag(np.ones((s))) + Lambda
+			c_vec = -alpha * np.dot(Y_inv,u)-np.dot(Y_inv,g)
+			Y_inv_delta_u = np.zeros(len(c_vec))
+			for j in xrange(len(c_vec)):
+				Y_inv_delta_u[j] = c_vec[j] / B[j,j]
+			delta_u = (-alpha * u - g - np.dot(M,np.dot(Y_inv.T,Y_inv_delta_u)))/(alpha+count2 * 1.)
+			count2 += 1
+		#print np.dot(delta_u.T,np.dot(K,delta_u))
+		u_old = u
 		u = u + delta_u
+		diff = np.abs(np.sum(u-u_old))
+		count1 += 1
 	return u
-	
+def calc_p_alpha(A,alpha,Cov,G,K,m):
+	inv_cov_squ = (np.diag(1./np.diagonal(Cov)))**2
+	d2_chi = np.dot(K.T,np.dot(inv_cov_squ,K))
+	mat = np.dot(np.diag(np.sqrt(A)),np.dot(d2_chi,np.diag(np.sqrt(A))))
+	eig,eigv = np.linalg.eig(mat)
+	S = np.sum(A - m - A * np.log(A / m))
+	print"S = ", S
+	L = 0.5 * np.sum((G - np.dot(K,A))**2/np.diagonal(Cov)**2)
+	Q = alpha * S - L
+	p_alpha_val = np.prod(np.sqrt(alpha/(alpha+eig)) * 1./alpha * np.exp(Q))
+	return p_alpha_val
